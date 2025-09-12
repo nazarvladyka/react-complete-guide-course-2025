3bd13dd02f099390682235f14445b674d629b18a
Nazar Vladyka <nazarvladyka@gmail.com>
2025-09-12 13:19:21 +0300

    Finished section about JS

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -2,4 +2,20 @@ import * as util from "./util.js";
 
 // alert(apiKey + ' ' + dummy);
 
+const userMessage = "Hello world!";
+
 console.log(util.apiKey);
+console.log(`hello`);
+
+
+function greetUser(userName, message) {
+    console.log(userName);
+    console.log(message);
+}
+
+const greetUser2 = (userName, message) => {
+    console.log(userName);
+    console.log(message);
+}
+
+greetUser("Nazar", "hello");
